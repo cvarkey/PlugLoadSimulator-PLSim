84faaadd03d695a2b91a841784fa334bd91f3891
Klint Segarra <ksegarra@uci.edu>
2016-04-04 15:36:17 -0700

    A lot of changes
    -restructured the program directories
    -added graph file outputs
    -made changes to how xml data is structured and parsed

--- inputstr_generator.py
+++ inputstr_generator.py
@@ -3,6 +3,13 @@ Input Generator class
 
 @author: Klint
 '''
+def make_input_generators(device_map: dict):
+    input_generators = []
+    
+    for k,v in device_map.items():
+        input_generators.append(InputGenerator(k, set(v.keys())))
+    
+    return input_generators
 
 class InputGenerator:
     
@@ -51,6 +58,43 @@ class InputGenerator:
         to_return += self._build_str()
         return to_return
 
+from collections import Counter
+
+'''
+Counter, d = {'a': 1, 'b':3}
+d['a'] == 1
+d['a'] += 11
+d['a'] == 12
+
+class NameGenerator:
+    add_word(s: string)->increment the count of that word if it exists, otherwise set count = 0
+
+    get_string(s: string)-> return s + count as string if 0, return s
+    
+    public void method(this, int x, int y)
+        a = (this.a)
+        method2 == (this.method2())
+'''
+
+
+class NameGenerator:
+    
+    def __init__(self):
+        self.counter = Counter()
+        
+    def add_word(self, s):
+        if s in self.counter:
+            self.counter[s] += 1
+        else:
+            self.counter[s] = 0
+    
+    def generate_name(self, s):
+        self.add_word(s)
+        if self.counter[s] == 0:
+            return s
+        else:
+            return s + ' ({})'.format(self.counter[s])
+        
 if __name__ == '__main__':
     i1 = InputGenerator('tv', {'on', 'off', 'standby'})
     
@@ -61,5 +105,12 @@ if __name__ == '__main__':
     
     i_str = i1.generate_str()
     print(i_str)
+    
+    n = NameGenerator()
+    print(n.generate_name('a'))
+    print(n.generate_name('a'))
+    
+    print(n.generate_name('b'))
+    print(n.generate_name('b'))
     
     
